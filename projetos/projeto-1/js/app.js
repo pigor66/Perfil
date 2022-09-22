0dd14b4094d5ca23776135909fa1a6e246ebae7b
Pigor <higorribeirosantos@gmail.com>
2022-09-22 20:00:53 -0300

    ajuste footer

--- projetos/projeto-1/js/app.js
+++ projetos/projeto-1/js/app.js
@@ -6,9 +6,7 @@ let usuario = [
     "higor", "teste@teste","123"
 ]
 // CADASTRO
-function cadastrar(nome, email, senha, confSenha) {
-    let dados = document.getElementById("dados")    
-    
+function cadastrar(nome, email, senha, confSenha) {    
     let postUsuario = [
         nome,email,senha,confSenha
     ]
@@ -25,25 +23,11 @@ function cadastrar(nome, email, senha, confSenha) {
             postUsuario[0].value, postUsuario[1].value, postUsuario[2].value, postUsuario[3].value
         ]
         console.log(usuario);
-        alert("Você foi cadastrado")
+        alert("Você foi cadastrado", postUsuario)
     }
 }
 
-function inserir(){
-    let row_2 = document.createElement('tr');
-    let row_2_data_1 = document.createElement('td');
-    row_2_data_1.innerHTML = usuario[0];
-    let row_2_data_2 = document.createElement('td');
-    row_2_data_2.innerHTML = usuario[1];
-    let row_2_data_3 = document.createElement('td');
-    row_2_data_3.innerHTML = usuario[2];
-    
-    row_2.appendChild(row_2_data_1);
-    row_2.appendChild(row_2_data_2);
-    row_2.appendChild(row_2_data_3);
-    tbody.appendChild(row_2);
-console.log(criarUsuario);
-}
+
 
 
 
