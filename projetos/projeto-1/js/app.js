07171ef1c216058f5746bae8f31c0662538299e9
Pigor <higorribeirosantos@gmail.com>
2022-09-23 20:33:13 -0300

    criação do card de login e armazenando dados de cadastro no local storage

--- projetos/projeto-1/js/app.js
+++ projetos/projeto-1/js/app.js
@@ -30,7 +30,7 @@ function cadastrar() {
 
         header.textContent = "Está faltando o Email..."
         body.textContent = "Tem que preencher todos os campos, inclusive seu Email."
-        btn.textContent = "Etcha, Esqueci"    
+        btn.textContent = "Etcha, Esqueci"
     } else if (!senha) {
         let header = document.getElementById('alertHeader')
         let body = document.getElementById('alertBody')
@@ -38,7 +38,7 @@ function cadastrar() {
 
         header.textContent = "Está faltando o senha..."
         body.textContent = "Para a asegurança das suas informações você deve colocar uma senha"
-        btn.textContent = "Desculpe, irei colocar"  
+        btn.textContent = "Desculpe, irei colocar"
     } else if (senha != confSenha) {
         let header = document.getElementById('alertHeader')
         let body = document.getElementById('alertBody')
@@ -47,52 +47,30 @@ function cadastrar() {
         header.textContent = "Algo esta errado"
         body.textContent = "Parece que as senhas nao batem"
         btn.textContent = "Vou refazer!"
-        } else if (nome && email && senha == confSenha) {
+    } else if (nome && email && senha == confSenha) {
 
-            let header = document.getElementById('alertHeader')
-            let body = document.getElementById('alertBody')
-            let btn = document.getElementById('closeAlert')
-    
-            header.textContent = "Maravilha"
-            body.textContent = "Agora você esta cadastrado"
-            btn.textContent = "Show"
+        let header = document.getElementById('alertHeader')
+        let body = document.getElementById('alertBody')
+        let btn = document.getElementById('closeAlert')
 
-        let tBody = document.getElementById('body')
+        header.textContent = "Maravilha"
+        body.textContent = "Agora você esta cadastrado"
+        btn.textContent = "Show"
+        
 
-        let usuarioLinha = document.createElement('tr');
-        let nomeColuna = document.createElement('td');
-        let emailColuna = document.createElement('td');
-        let senhaColuna = document.createElement('td');
+        localStorage.setItem("nome", nome) 
+        localStorage.setItem("email", email) 
+        localStorage.setItem("senha",senha) 
 
-        nomeColuna.textContent = nome
-        emailColuna.textContent = email
-        senhaColuna.textContent = senha
-
-        usuarioLinha.appendChild(nomeColuna);
-        usuarioLinha.appendChild(emailColuna);
-        usuarioLinha.appendChild(senhaColuna);
-
-        tBody.appendChild(usuarioLinha);
-
-        tBody.nome    }
-    let body = document.getElementById('body');
-
-
-    console.log(body.children);
-
-
-    // console.log(usuarioLinha);
-
-
-}
+      
 
 
 
 
-function pegueiTbody() {
-    let td = document.getElementsByTagName('td');
+    }
 
-    console.log(td);
+
+
 }
 
 
